29ec02f73fa1d1c8fd5cde827d1318faca0dadde
Abdulkadir Durmaz <43341682+cyrionp@users.noreply.github.com>
2023-08-04 17:50:24 +0300

    finished: navbar menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import NavBar from "@/scenes/navbar";
+import { useState } from "react";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>('home');
+
   return (
     <div className='app bg-gray-20'>
-      app
+      <NavBar 
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,31 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props={
+    page: string
+    selectedPage: string
+    setSelectedPage: (value: string) => void;
+}
+
+const Link = ({
+    page,
+    selectedPage,
+    setSelectedPage,
+}: Props) => {
+    // Links must be in lowercase.
+    // If a page's name includes space, this regex deletes it.
+    const lowerCasePage = page.toLowerCase().replace(/ /g, "")
+
+    return (
+        <AnchorLink
+            className={`${selectedPage===lowerCasePage ? "text-primary-500":""}
+                transition duration-500 hover:text-primary-300
+            `}
+            href={`#${lowerCasePage}`}
+            onClick={()=>setSelectedPage(lowerCasePage)}
+        >
+            {page}
+        </AnchorLink>
+    );
+}
+ 
+export default Link;
